b1373942206da994af6dcdbfbc37a9b18a228321
Sviatoslav Yezhelyi <svyatoslav.fe@gmail.com>
2024-06-25 16:19:14 +0300

    chore: Update import paths for header component in about and home pages

--- app/about/page.js
+++ app/about/page.js
@@ -1,7 +1,10 @@
+import Header from "@/components/header";
+
 export default function About() {
   console.log("About executed");
   return (
     <main>
+        <Header />
       <h1>About</h1>
       <p>This is the about page</p>
     </main>
