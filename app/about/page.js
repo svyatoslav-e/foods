4c7df44a56b32d33ffdbd984f164505d3d03701e
Sviatoslav Yezhelyi <svyatoslav.fe@gmail.com>
2024-06-25 14:02:22 +0300

    about page

---
+++ app/about/page.js
@@ -0,0 +1,9 @@
+export default function About() {
+  console.log("About executed");
+  return (
+    <main>
+      <h1>About</h1>
+      <p>This is the about page</p>
+    </main>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+
 export default function Home() {
   console.log('Home executed')
   return (
@@ -5,6 +7,12 @@ export default function Home() {
       <img src="/logo.png" alt="A server surrounded by magic sparkles." />
       <h1>Welcome to this NextJS Course!</h1>
       <p>🔥 Let&apos;s get started! 🔥</p>
+      <p>
+        {/* <a href="/about">About</a> */}
+        <Link href="/about">
+          About
+        </Link>
+      </p>
     </main>
   );
 }
