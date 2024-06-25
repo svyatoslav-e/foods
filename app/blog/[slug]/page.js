5f7150d77fd45cc05d711c6588c712928e434516
Sviatoslav Yezhelyi <svyatoslav.fe@gmail.com>
2024-06-25 16:33:42 +0300

    chore: Update blog post page to display dynamic slug parameter

--- app/blog/[slug]/page.js
+++ app/blog/[slug]/page.js
@@ -1,8 +1,9 @@
-export default function BlogPost() {
+export default function BlogPost({ params }) {
+    console.log("BlogPost executed: ", params);
     return (
         <main>
             <h1>Post 1</h1>
-            <p>This is post page</p>
+            <p>This is { params.slug } page</p>
         </main>
     );
 }
