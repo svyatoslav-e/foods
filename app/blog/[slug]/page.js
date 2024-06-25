e1f8a1da6714d86f20f0316d4f64f2645e24a4d8
Sviatoslav Yezhelyi <svyatoslav.fe@gmail.com>
2024-06-25 16:35:33 +0300

    chore: Remove unused code and update page layout

--- app/blog/[slug]/page.js
+++
@@ -1,9 +0,0 @@
-export default function BlogPost({ params }) {
-    console.log("BlogPost executed: ", params);
-    return (
-        <main>
-            <h1>Post 1</h1>
-            <p>This is { params.slug } page</p>
-        </main>
-    );
-}
--- app/page.js
+++ app/page.js
@@ -1,18 +1,9 @@
-import Link from 'next/link';
-import Header from "@/components/header";
-
 export default function Home() {
-  console.log('Home executed')
   return (
     <main>
-      <Header />
-      <p>🔥 Let&apos;s get started! 🔥</p>
-      <p>
-        {/* <a href="/about">About</a> */}
-        <Link href="/about">
-          About
-        </Link>
-      </p>
+      <h1 style={{ color: 'white', textAlign: 'center' }}>
+        Time to get started!
+      </h1>
     </main>
   );
 }
